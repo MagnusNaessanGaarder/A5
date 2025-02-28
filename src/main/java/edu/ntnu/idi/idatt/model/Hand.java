1db9fd378de9911029ad445fabd4c5dd1495271b
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-02-28 12:21:16 +0100

    creating hand-class for deck

---
+++ src/main/java/edu/ntnu/idi/idatt/model/Hand.java
@@ -0,0 +1,47 @@
+package edu.ntnu.idi.idatt.model;
+
+import java.util.Arrays;
+import javax.smartcardio.CardException;
+
+public class Hand {
+  private PlayingCard[] hand = new PlayingCard[5];
+  public Hand() {
+    hand = newHand();
+  }
+
+  public PlayingCard[] newHand() {
+    DeckOfCards deck = new DeckOfCards();
+    return deck.dealHand(hand.length);
+  }
+
+  public PlayingCard[] getHand() {
+    return hand;
+  }
+
+  public boolean hasFlush() {
+    try {
+      String firstSuitColor = checkColor(getHand()[0]);
+      return Arrays.stream(hand).allMatch(c -> {
+        try {
+          return checkColor(c).equals(firstSuitColor);
+        } catch (CardException e) {
+          e.printStackTrace();
+          return false;
+        }
+      });
+    } catch (CardException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public String checkColor(PlayingCard card) throws CardException {
+    if (card.getSuitChar() == 'C' || card.getSuitChar() == 'S') {
+      return "Black";
+    } else if (card.getSuitChar() == 'H' || card.getSuitChar() == 'D') {
+      return "Red";
+    } else {
+      throw new CardException("Invalid suit");
+    }
+  }
+}
