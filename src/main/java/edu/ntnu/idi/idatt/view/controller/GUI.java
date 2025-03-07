0212c34d1c65af7ab1d7c37a54d45f526ca47ba7
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-03-07 10:41:38 +0100

    updated gui

--- src/main/java/edu/ntnu/idi/idatt/view/controller/GUI.java
+++ src/main/java/edu/ntnu/idi/idatt/view/controller/GUI.java
@@ -7,17 +7,37 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
   private final Group root = new Group();
+  private Stage primaryStage;
 
-  public void init(Stage primaryStage) {
-    primaryStage = new Stage();
+  public void init(Stage stage) {
+    this.primaryStage = stage;
     Scene scene = new Scene(root, 640, 480);
-    primaryStage.setTitle("Flush Game");
-    primaryStage.setScene(scene);
+    this.primaryStage.setTitle("Flush Game");
+    this.primaryStage.setScene(scene);
   }
 
   @Override
   public void start(Stage primaryStage) {
     init(primaryStage);
-    primaryStage.show();
+    this.primaryStage.show();
+  }
+
+  private void display() {
+    // Display the GUI
+    displayCards();
+    displayActionBar();
+    displaySidebarMenu();
+  }
+
+  private void displayCards() {
+    // Display the cards
+  }
+
+  private void displayActionBar() {
+    // Display the action bar
+  }
+
+  private void displaySidebarMenu() {
+    // Display the sidebar menu
   }
 }
