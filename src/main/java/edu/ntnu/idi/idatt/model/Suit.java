d7d9d5e3b8404534481f245dada9e5cee9ff599c
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-03-07 10:41:27 +0100

    Updated enum for displaying a suit

---
+++ src/main/java/edu/ntnu/idi/idatt/model/Suit.java
@@ -0,0 +1,81 @@
+package edu.ntnu.idi.idatt.model;
+
+public enum Suit {
+  HEARTS {
+    @Override
+    public boolean isRed() {
+      return true;
+    }
+    @Override
+    public char getSuitSymbol() {
+        return '♥';
+    }
+    @Override
+    public char getSuitChar() {
+        return 'H';
+    }
+    @Override
+    public String getSuitName() {
+        return "Hearts";
+    }
+  },
+  DIAMONDS {
+      @Override
+      public boolean isRed() {
+      return true;
+      }
+      @Override
+      public char getSuitSymbol() {
+          return '♦';
+      }
+      @Override
+      public char getSuitChar() {
+          return 'D';
+      }
+      @Override
+      public String getSuitName() {
+          return "Diamonds";
+      }
+  },
+  CLUBS {
+      @Override
+      public boolean isRed() {
+      return false;
+      }
+      @Override
+      public char getSuitSymbol() {
+          return '♣';
+      }
+      @Override
+      public char getSuitChar() {
+          return 'C';
+      }
+      @Override
+      public String getSuitName() {
+          return "Clubs";
+      }
+  },
+  SPADES {
+      @Override
+      public boolean isRed() {
+      return false;
+      }
+      @Override
+      public char getSuitSymbol() {
+          return '♠';
+      }
+      @Override
+      public char getSuitChar() {
+          return 'S';
+      }
+      @Override
+      public String getSuitName() {
+          return "Spades";
+      }
+  };
+
+  public abstract boolean isRed();
+  public abstract char getSuitSymbol();
+  public abstract char getSuitChar();
+  public abstract String getSuitName();
+}
