31e92c0b69df126f067d2021ef2a4e16dd7eb98c
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-03-08 02:17:03 +0100

    updated model info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module edu.ntnu.idi.idatt {
   requires java.smartcardio;
   opens edu.ntnu.idi.idatt.view.controller to javafx.graphics;
   exports edu.ntnu.idi.idatt.app;
+  opens edu.ntnu.idi.idatt.view to javafx.graphics;
+  opens edu.ntnu.idi.idatt.view.window to javafx.graphics;
+  opens edu.ntnu.idi.idatt.view.window.components to javafx.graphics;
+    opens edu.ntnu.idi.idatt.model to javafx.graphics;
 }
