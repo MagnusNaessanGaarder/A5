8bcfecd2708c54d553da8bac14d7a3fec884809e
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2025-03-10 00:06:44 +0100

    Refactoring and updating logic

--- src/main/java/edu/ntnu/idi/idatt/model/DeckOfCards.java
+++ src/main/java/edu/ntnu/idi/idatt/model/DeckOfCards.java
@@ -7,29 +7,38 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DeckOfCards {
-  private final char[] suit = {'S', 'H', 'D', 'C'};
+  private final char[] validSuit = {'S', 'H', 'D', 'C'};
   private PlayingCard[] cards;
 
   public DeckOfCards() {
     cards = generateDeck();
   }
 
-  private PlayingCard[] generateDeck() {
-    return IntStream.range(0, suit.length) // Iterate over suit indices
-        .mapToObj(i -> suit[i]) // Convert indices to suit chars
+  public  PlayingCard[] getDeck() {
+    return cards;
+  }
+
+  public PlayingCard[] generateDeck() {
+    return IntStream.range(0, validSuit.length) // Iterate over suit indices
+        .mapToObj(i -> validSuit[i]) // Convert indices to suit chars
         .flatMap(s -> IntStream.rangeClosed(1, 13) // Iterate over ranks 1-13
             .mapToObj(rank -> new PlayingCard(s, rank))) // Create PlayingCard
         .toArray(PlayingCard[]::new);
   }
 
   public PlayingCard[] dealHand(int n) {
-    if (n < 0 || n > 52) {
+    if (n <= 0 || n > 52) {
       throw new IllegalArgumentException("Invalid number of cards requested");
     }
     shuffle();
 
     return Arrays.stream(cards, 0, n)
         .toArray(PlayingCard[]::new);
+  }
+
+  public DeckOfCards debugShuffle() {
+    shuffle();
+    return this;
   }
 
   private void shuffle() {
@@ -39,5 +48,22 @@ public class DeckOfCards {
     Collections.shuffle(shuffledList); // Shuffle using built-in method
 
     cards = shuffledList.toArray(new PlayingCard[0]); // Convert back to array
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DeckOfCards otherCard = (DeckOfCards) o;
+    for (int i = 0; i < cards.length; i++) {
+      if (!otherCard.getDeck()[i].equals(cards[i])) {
+        return false;
+      }
+    }
+    return true;
   }
 }
